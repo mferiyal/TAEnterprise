aa57bc6c4524bb76440640eab5d59d3592a2023a
diktaafif <afifdikta@gmail.com>
2018-12-02 22:49:18 +0700

    tambahan titik nang daftar
    asdasd

--- app/src/main/java/com/mfir/pc/taenterprise/Daftar.java
+++ app/src/main/java/com/mfir/pc/taenterprise/Daftar.java
@@ -1,13 +1,27 @@
 package com.mfir.pc.taenterprise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Daftar extends AppCompatActivity {
 
+    Button btn_daftar;
+    EditText Edtnama,EdtEmail,EdtPassword;
+
+    private void goToActivity(){
+        Intent mIntent = new Intent(getApplicationContext(), Login.class);
+        startActivity(mIntent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
+        btn_daftar = findViewById(R.id.ButtonDaftarDiLogin);
+        Edtnama =findViewById(R.id.editDaftarNama);
+        EdtEmail =findViewById(R.id.editDaftarEmail);
+        EdtPassword =findViewById(R.id.editDaftarPassword);
     }
 }
