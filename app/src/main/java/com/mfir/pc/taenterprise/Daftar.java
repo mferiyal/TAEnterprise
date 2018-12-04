d0c298000227506a0b641053137708967b4de2a8
diktaafif <afifdikta@gmail.com>
2018-12-04 21:06:59 +0700

    perubhan
    tak edit i lgin e terus tak intent urung tak benakno recycler e

--- app/src/main/java/com/mfir/pc/taenterprise/Daftar.java
+++ app/src/main/java/com/mfir/pc/taenterprise/Daftar.java
@@ -3,6 +3,7 @@ package com.mfir.pc.taenterprise;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,5 +24,14 @@ public class Daftar extends AppCompatActivity {
         Edtnama =findViewById(R.id.editDaftarNama);
         EdtEmail =findViewById(R.id.editDaftarEmail);
         EdtPassword =findViewById(R.id.editDaftarPassword);
+
+        btn_daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
