53d7fd9ae72616c44a8b3e5adff3b645ada3857c
mferiyal <44905071+mferiyal@users.noreply.github.com>
2018-12-11 20:27:49 +0700

    api
    api

--- app/src/main/java/com/mfir/pc/taenterprise/Rest/ApiInterface.java
+++ app/src/main/java/com/mfir/pc/taenterprise/Rest/ApiInterface.java
@@ -1,34 +1,35 @@
 package com.mfir.pc.taenterprise.Rest;
 
+import com.mfir.pc.taenterprise.Model.ResultArtikel;
+import com.mfir.pc.taenterprise.Model.ResultUser;
+
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 
 public interface ApiInterface {
-    //    Login
     @FormUrlEncoded
-    @POST("Login")
-    Call<> getLogin(@Field("nohp") String nohp,
-                                       @Field("password") String password);
+    @POST("ruangan/login")
+    Call<ResultUser> loginRequest(@Field("username")String username);
 
-    //    Kendaraan
-    @GET("Kendaraan")
-    Call<GetKendaraan> getKendaraan();
+    @GET("ruangan/ruangan")
+    Call<ResultArtikel> getRuangan();
 
-    @FormUrlEncoded
-    @POST("Kendaraan")
-    Call<PostPutDellKendaraan> postKendaraan(@Field("nama") String nama,
-                                             @Field("harga") String harga,
-                                             @Field("img") String img);
-    @FormUrlEncoded
-    @PUT("Kendaraan")
-    Call<PostPutDellKendaraan> putKendaraan(@Field("id") String id,
-                                            @Field("nama") String nama,
-                                            @Field("harga") String harga,
-                                            @Field("img") String img);
+    @Multipart
+    @POST("ruangan/login")
+    Call<ResultUser> postLogin(@Part("username") RequestBody username,
+                               @Part("password")RequestBody password);
+
+//    @Multipart
+//    @POST("ruangan/login")
+//    Call<ResultUser> postLogin(@Part("username")RequestBody username,
+//                               @Part("password")RequestBody password);
 }
 
